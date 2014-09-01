ba419af99adddb9d5574cc77ad0ed15761f65308
Gregory Jacobs <greg@greg-jacobs.com>
2014-09-01 19:43:30 -0400

    Add text to slides

--- app/js/controllers/MainCtrl.js
+++ app/js/controllers/MainCtrl.js
@@ -3,12 +3,12 @@ angular.module( 'CarouselPoc' ).controller( 'MainCtrl', function( $scope ) {
 	$scope.index = 0;
 	
 	$scope.images = [
-		'img/329_wpm_lowres.jpg',
-		'img/380_wpm_lowres.jpg',
-		'img/1574_wpm_lowres.jpg',
-		'img/6076_wpm_hires.jpg',
-		'img/14724_wpm_lowres.jpg',
-		'img/14758_wpm_lowres.jpg'
+		{ url: 'img/329_wpm_lowres.jpg', thumbUrl: 'img/329_wpm_lowres.jpg', description: "Movin' on up" },
+		{ url: 'img/380_wpm_lowres.jpg', thumbUrl: 'img/380_wpm_lowres.jpg', description: "Computing in the modern world	" },
+		{ url: 'img/6076_wpm_hires.jpg', thumbUrl: 'img/6076_wpm_hires.jpg', description: "Close up" },
+		{ url: 'img/14724_wpm_lowres.jpg', thumbUrl: 'img/14724_wpm_lowres.jpg', description: "Energy" },
+		{ url: 'img/1574_wpm_lowres.jpg', thumbUrl: 'img/1574_wpm_lowres.jpg', description: "Communicate" },
+		{ url: 'img/14758_wpm_lowres.jpg', thumbUrl: 'img/14758_wpm_lowres.jpg', description: "Sleek Finesse" }
 	];
 	
 	$scope.updateIndex = function( idx ) {
