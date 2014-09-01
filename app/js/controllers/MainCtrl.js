2859c7441fc09750e46f471a12092e182b140732
Gregory Jacobs <greg@greg-jacobs.com>
2014-09-01 17:09:57 -0400

    Initial commit

---
+++ app/js/controllers/MainCtrl.js
@@ -0,0 +1,14 @@
+angular.module( 'CarouselPoc' ).controller( 'MainCtrl', function( $scope ) {
+	
+	$scope.index = 0;
+	
+	$scope.images = [
+		'img/329_wpm_lowres.jpg',
+		'img/380_wpm_lowres.jpg',
+		'img/1574_wpm_lowres.jpg',
+		'img/6076_wpm_hires.jpg',
+		'img/14724_wpm_lowres.jpg',
+		'img/14758_wpm_lowres.jpg'
+	];
+	
+} );
