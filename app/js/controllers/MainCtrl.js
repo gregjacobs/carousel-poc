827efdefb277a0cf1c5deb5adff964ff9cd7c2a9
Gregory Jacobs <greg@greg-jacobs.com>
2014-09-01 21:58:24 -0400

    Remove one img

--- app/js/controllers/MainCtrl.js
+++ app/js/controllers/MainCtrl.js
@@ -7,8 +7,7 @@ angular.module( 'CarouselPoc' ).controller( 'MainCtrl', function( $scope ) {
 		{ url: 'img/380_wpm_lowres.jpg', thumbUrl: 'img/380_wpm_lowres.jpg', description: "Computing in the modern world" },
 		{ url: 'img/6076_wpm_hires.jpg', thumbUrl: 'img/6076_wpm_hires.jpg', description: "Close up" },
 		{ url: 'img/14724_wpm_lowres.jpg', thumbUrl: 'img/14724_wpm_lowres.jpg', description: "Energy" },
-		{ url: 'img/1574_wpm_lowres.jpg', thumbUrl: 'img/1574_wpm_lowres.jpg', description: "Communicate" },
-		{ url: 'img/14758_wpm_lowres.jpg', thumbUrl: 'img/14758_wpm_lowres.jpg', description: "Sleek Finesse" }
+		{ url: 'img/1574_wpm_lowres.jpg', thumbUrl: 'img/1574_wpm_lowres.jpg', description: "Communicate" }
 	];
 	
 } );
